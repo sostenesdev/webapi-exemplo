85929104b9ef2f214aeb90d1a234131c4a52e285
Sóstenes Dias de Oliveira Junior <sostenes.dev@mail.com>
2020-11-26 01:17:44 -0300

    Add DbContext

---
+++ Sostenes.MyApi/Sostenes.MyApi.AcessoDados.Entity/Context/MyApiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Sostenes.MyApi.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sostenes.MyApi.AcessoDados.Entity.Context
+{
+    class MyApiDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+
+        public MyApiDbContext()
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
